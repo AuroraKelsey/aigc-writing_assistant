3dfd24e8239d1c00f1da9bf42319246f9bc8c13d
kelsey-win <15633677687@163.com>
2025-06-20 10:58:56 +0800

    文言文界面与登录界面

--- app.py
+++ app.py
@@ -1,23 +1,32 @@
 from flask import Flask, render_template
 from chinesePage import chinese_page
+from classicalChinesePage import classical_chinese_page
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 # 注册诗词写作页面蓝图
-app.register_blueprint(chinese_page, url_prefix='/')
+app.register_blueprint(chinese_page, url_prefix="/")
 
 
-@app.route('/englishPage')
-def englishPage():
-    return render_template('englishPage.html')
+# 注册文言文分析页面蓝图
+app.register_blueprint(classical_chinese_page, url_prefix="/")
 
-@app.route('/draw')
+
+@app.route("/classicalChinese")
+def classicalChinese():
+    return render_template("classicalChinese.html")
+
+
+@app.route("/draw")
 def draw():
-    return render_template('draw.html')
+    return render_template("draw.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
